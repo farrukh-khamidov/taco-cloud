a4e2cf157b6558c800cce9af2f55dcadfd8139e9
Farrukh <farrukh.khamidov@upay.uz>
2022-10-04 09:17:24 +0500

    order added

--- src/main/java/tacos/controllers/DesignTacoController.java
+++ src/main/java/tacos/controllers/DesignTacoController.java
@@ -1,22 +1,25 @@
 package tacos.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import tacos.constants.IngredientType;
 import tacos.models.Ingredient;
 import tacos.models.Taco;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Component
+@Controller
 @RequestMapping("/design")
+@Slf4j
 public class DesignTacoController {
     @GetMapping
     public String showDesignForm(Model model) {
@@ -50,10 +53,32 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(Taco taco) {
+    public String processDesign(@Valid Taco taco, Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            List<Ingredient> ingredients = Arrays.asList(
+                    new Ingredient("FLTO", "Flour Tortilla", IngredientType.WRAP),
+                    new Ingredient("COTO", "Corn Tortilla", IngredientType.WRAP),
+                    new Ingredient("ASD", "SDFA ascfsafcas", IngredientType.WRAP),
+                    new Ingredient("GRBF", "Ground Beef", IngredientType.PROTEIN),
+                    new Ingredient("CARN", "Carnitas", IngredientType.PROTEIN),
+                    new Ingredient("TMTO", "Diced Tomatoes", IngredientType.VEGGIES),
+                    new Ingredient("LETC", "Lettuce", IngredientType.VEGGIES),
+                    new Ingredient("CHED", "Cheddar", IngredientType.CHEESE),
+                    new Ingredient("JACK", "Monterrey Jack", IngredientType.CHEESE),
+                    new Ingredient("SLSA", "Salsa", IngredientType.SAUCE),
+                    new Ingredient("SRCR", "Sour Cream", IngredientType.SAUCE)
+            );
+            IngredientType[] types = IngredientType.values();
+            for (IngredientType type : types) {
+                model.addAttribute(type.toString().toLowerCase(),
+                        filterByType(ingredients, type));
+            }
+            return "design";
+        }
         // Save the taco design...
         // We'll do this in chapter 3
-        System.out.println("taco: " + taco);
-        return "redirect:/";
+        log.info("taco: " + taco);
+        return "redirect:/orders/current";
     }
 }
---
+++ src/main/java/tacos/controllers/OrderController.java
@@ -0,0 +1,42 @@
+package tacos.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tacos.constants.IngredientType;
+import tacos.models.Ingredient;
+import tacos.models.Order;
+import tacos.models.Taco;
+
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm(Model model) {
+        model.addAttribute("order", new Order());
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(@Valid Order order, Errors errors) {
+
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
+        log.info("Order submitted: " + order);
+        return "redirect:/";
+    }
+}
