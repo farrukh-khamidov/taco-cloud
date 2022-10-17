87fdeb3d95d4681e58218bd43c399ab629234610
Farrukh <farrukh.khamidov@upay.uz>
2022-10-17 15:32:10 +0500

    jpa implementation done

--- src/main/java/tacos/repositories/OrderRepository.java
+++ src/main/java/tacos/repositories/OrderRepository.java
@@ -1,16 +1,12 @@
 package tacos.repositories;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import tacos.entities.Order;
 
 import java.util.Date;
 import java.util.List;
-
 public interface OrderRepository extends CrudRepository<Order, Long> {
     List<Order> findAllByZip(String zip);
-
-    @Query("SELECT Order from Order o WHERE o.zip = ?1 and o.placedAt between ?2 and ?3")
     List<Order> findAllByZipAndPlacedAtBetween(String zip, Date startDate, Date endDate);
 
     // select * from orders where zip = ? and placedAt between ? and ?
