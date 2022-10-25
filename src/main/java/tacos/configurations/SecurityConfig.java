ceb21aaa32aa9d5286ae3a4026f325fc85fc4968
Farrukh <farrukh.khamidov@upay.uz>
2022-10-25 07:40:08 +0500

    ldap user store added

--- src/main/java/tacos/configurations/SecurityConfig.java
+++ src/main/java/tacos/configurations/SecurityConfig.java
@@ -1,50 +1,96 @@
 package tacos.configurations;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    DataSource dataSource;
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-//        auth.jdbcAuthentication()
-//                .dataSource(dataSource);
+//        auth
+//                .ldapAuthentication()
+//                .userSearchFilter("(uid={0})")
+//                .groupSearchFilter("member={0}");
 
 
 
 //        auth
-//                .jdbcAuthentication()
-//                .dataSource(dataSource)
-//                .usersByUsernameQuery(
-//                        "select username, password, enabled from Users " +
-//                                "where username=?")
-//                .authoritiesByUsernameQuery(
-//                        "select username, authority from UserAuthorities " +
-//                                "where username=?");
+//                .ldapAuthentication()
+//                .userSearchBase("ou=people")
+//                .userSearchFilter("(uid={0})")
+//                .groupSearchBase("ou=groups")
+//                .groupSearchFilter("member={0}");
 
 
-        auth
-                .jdbcAuthentication()
-                .dataSource(dataSource)
-                .usersByUsernameQuery(
-                        "select username, password, enabled from Users " +
-                                "where username=?")
-                .authoritiesByUsernameQuery(
-                        "select username, authority from UserAuthorities " +
-                                "where username=?")
-                .passwordEncoder(new StandardPasswordEncoder("53cr3t"));
+//        auth
+//                .ldapAuthentication()
+//                .userSearchBase("ou=people")
+//                .userSearchFilter("(uid={0})")
+//                .groupSearchBase("ou=groups")
+//                .groupSearchFilter("member={0}")
+//                .passwordCompare();
+
+
+//        auth
+//                .ldapAuthentication()
+//                .userSearchBase("ou=people")
+//                .userSearchFilter("(uid={0})")
+//                .groupSearchBase("ou=groups")
+//                .groupSearchFilter("member={0}")
+//                .passwordCompare()
+//                .passwordEncoder(new BCryptPasswordEncoder())
+//                .passwordAttribute("passcode");
+
+
+
+
+
+//
+//        auth
+//                .ldapAuthentication()
+//                .userSearchBase("ou=people")
+//                .userSearchFilter("(uid={0})")
+//                .groupSearchBase("ou=groups")
+//                .groupSearchFilter("member={0}")
+//                .passwordCompare()
+//                .passwordEncoder(new BCryptPasswordEncoder())
+//                .passwordAttribute("passcode")
+//                .contextSource()
+//                .url("ldap://tacocloud.com:389/dc=tacocloud,dc=com");
+
+
+
+//        auth
+//                .ldapAuthentication()
+//                .userSearchBase("ou=people")
+//                .userSearchFilter("(uid={0})")
+//                .groupSearchBase("ou=groups")
+//                .groupSearchFilter("member={0}")
+//                .passwordCompare()
+//                .passwordEncoder(new BCryptPasswordEncoder())
+//                .passwordAttribute("passcode")
+//                .contextSource()
+//                .root("dc=tacocloud,dc=com");
+
+//        auth
+//                .ldapAuthentication()
+//                .userSearchBase("ou=people")
+//                .userSearchFilter("(uid={0})")
+//                .groupSearchBase("ou=groups")
+//                .groupSearchFilter("member={0}")
+//                .passwordCompare()
+//                .passwordEncoder(new BCryptPasswordEncoder())
+//                .passwordAttribute("passcode")
+//                .contextSource()
+//                .root("dc=tacocloud,dc=com")
+//                .ldif("classpath:users.ldif");
 
 
     }
