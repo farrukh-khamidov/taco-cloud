706b526cf891d59400808c79f8c32c0f636f2d6e
Farrukh <farrukh.khamidov@upay.uz>
2022-11-08 07:57:19 +0500

    property environment added

--- src/main/java/tacos/controllers/OrderController.java
+++ src/main/java/tacos/controllers/OrderController.java
@@ -2,10 +2,16 @@ package tacos.controllers;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
+import tacos.components.OrderProps;
 import tacos.entities.Order;
 import tacos.entities.User;
 import tacos.repositories.OrderRepository;
@@ -19,11 +25,32 @@ import java.security.Principal;
 @RequestMapping("/orders")
 @AllArgsConstructor
 @SessionAttributes("order")
+//@ConfigurationProperties(prefix="taco.orders")
 public class OrderController {
 
 
     private OrderRepository orderRepository;
     private UserRepository userRepository;
+
+    private OrderProps props;
+
+//    private int pageSize = 20;
+//    public void setPageSize(int pageSize) {
+//        this.pageSize = pageSize;
+//    }
+
+//    @GetMapping
+//    public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+//        model.addAttribute("orders", orderRepository.findByUserOrderByPlacedAtDesc(user));
+//        return "orderList";
+//    }
+
+    @GetMapping
+    public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, props.getPageSize());
+        model.addAttribute("orders", orderRepository.findByUserOrderByPlacedAtDesc(user, pageable));
+        return "orderList";
+    }
 
     @GetMapping("/current")
     public String orderForm() {
@@ -38,8 +65,7 @@ public class OrderController {
             return "orderForm";
         }
 
-        User user = userRepository.findByUsername(
-                principal.getName()).get();
+        User user = userRepository.findByUsername(principal.getName()).get();
         order.setUser(user);
 
         orderRepository.save(order);
--- src/main/java/tacos/repositories/OrderRepository.java
+++ src/main/java/tacos/repositories/OrderRepository.java
@@ -1,13 +1,18 @@
 package tacos.repositories;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tacos.entities.Order;
+import tacos.entities.User;
 
 import java.util.Date;
 import java.util.List;
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findAllByZip(String zip);
     List<Order> findAllByZipAndPlacedAtBetween(String zip, Date startDate, Date endDate);
+
+    List<Order> findByUserOrderByPlacedAtDesc(User user);
+    List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 
     // select * from orders where zip = ? and placedAt between ? and ?
     // jpql
